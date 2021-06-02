ffa6b18fbb99c8b46d35856e45e60763c7ba65e8
Ted Irland <irlandth@gmail.com>
2021-06-02 10:40:52 -0500

    Added a select field and walked through data modeling for those via the v-model directive

--- web-forms/src/components/SignupForm.vue
+++ web-forms/src/components/SignupForm.vue
@@ -5,9 +5,16 @@
     <label>Password: </label>
     <input type="password" required v-model="password">
 
+    <label>Role:</label>
+    <select v-model="role">
+      <option value='developer'>Web Developer</option>
+      <option value='designer'>Web Designer</option>
+    </select>
+
   </form>
     <p>Email: {{email}}</p>
     <p>Password: {{password}}</p>
+    <p>Role: {{role}}</p>
 </template>
 
 <script>
@@ -16,6 +23,7 @@ export default {
     return {
       email: 'newemail',
       password: '',
+      role: 'designer'
     }
   }
 
@@ -42,7 +50,7 @@ label {
   font-weight: bold;
 }
 
-input {
+input, select {
   display: block;
   padding: 10px 6px;
   width: 100%;
